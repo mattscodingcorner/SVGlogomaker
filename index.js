e93bf730df466024d0d2883b951e10aa1683f070
unknown <matt.j.velazquez@gmail.com>
2023-08-24 14:55:44 -0700

    fixed main logic and userinput

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const getUserInput = require('./lib/userInput.js');
 const generateSVG = require('./lib/svgGenerator.js');
 const { Triangle, Circle, Square } = require('./lib/shapes.js');
+const {getUserInput} = require("./lib/userInput.js");
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-function main() {
-    getUserInput((userInput) => {
-        let shape;
+async function main() {
+    const {shape, color, text} = await inquirer.prompt(getUserInput) 
+        let objectShape;
 
-        switch (userInput.shape) {
+        switch (shape) {
             case 'triangle':
-                shape = new Triangle();
+                objectShape = new Triangle();
                 break;
             case 'circle':
-                shape = new Circle();
+                objectShape = new Circle();
                 break;
             case 'square':
-                shape = new Square();
+                objectShape = new Square();
                 break;
             default:
                 console.log('Invalid shape choice');
                 return;
         }
 
-        shape.setColor(userInput.color);
-        const svgContent = generateSVG(shape, userInput.color, userInput.text);
+        objectShape.setColor(color);
+        const svgContent = generateSVG(objectShape, color, text);
 
         // Save the SVG content to a file
         fs.writeFile('output.svg', svgContent, (err) => {
@@ -35,7 +37,7 @@ function main() {
 
         console.log('Logo generated and saved!');
         });
-    });
-}
+    };
+
 
 main();
